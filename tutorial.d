7b517ac595cebd6f4392df2c73511e5cb6bfc3d9
Jacque Neuville <jacque.neuville@maildu.de>
2015-11-01 20:17:50 +0100

    String concatenation

--- tutorial.d
+++ tutorial.d
@@ -4,9 +4,6 @@ import std.stdio;
 
 void main() {
 
-    writeln(int.init);
-    writeln(char.init);
-    writeln(string.init);
-    writeln(bool.init);
+    writeln("Hello" ~ "," ~ " " ~ "World" ~ "!");
 
 }
