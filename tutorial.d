1b840a61a496d8de5a6d6d6934d63a2388e13741
Jacque Neuville <jacque.neuville@maildu.de>
2015-11-01 20:15:36 +0100

    Assertion failure

--- tutorial.d
+++ tutorial.d
@@ -2,16 +2,8 @@ module tutorial;
 
 import std.stdio;
 
-struct Person
-{
-    string firstName;
-    string lastName;
-}
-
 void main() {
 
-	Person person;
-	person.firstName = "Jacque";
-	person.lastName = "Neuville";
+    assert(false);
 
 }
